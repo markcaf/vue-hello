3a59978627d215bea5ac7e6c1c68ff34359d1724
markcaf <marcocafassog@gmail.com>
2022-07-04 17:16:57 +0200

    Two buttons with methods to change the image

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ const app = new Vue(
         data: {
             message: 'Hello World, scrivi e cambia messaggio',
             sourceImage: 'https://picsum.photos/200',
+            anotherImage: 'https://picsum.photos/300',
+            anotherSmallImage: 'https://picsum.photos/200'
+        },
+
+        methods: {
+            updateImage: function () {
+                this.sourceImage = this.anotherImage;
+            },
+
+            backToSmallerImage: function () {
+                this.sourceImage = this.anotherSmallImage;
+            }
         }
     }
 );
